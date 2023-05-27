c6469a44495e7c6d98c81dda7ea370745f9699a4
bianyishupian <z17866920355@163.com>
2023-05-27 22:06:12 +0800

    fix bug

--- include/server/db/db.h
+++ include/server/db/db.h
@@ -21,29 +21,29 @@ class MySQL
     // 初始化数据库连接
     MySQL()
     {
-        _conn = mysql_init(nullptr);
+        m_conn = mysql_init(nullptr);
     }
     // 释放数据库连接资源
     ~MySQL()
     {
-        if (_conn != nullptr)
-            mysql_close(_conn);
+        if (m_conn != nullptr)
+            mysql_close(m_conn);
     }
     // 连接数据库
     bool connect()
     {
-         MYSQL *p = mysql_real_connect(_conn, server.c_str(), user.c_str(),
+         MYSQL *p = mysql_real_connect(m_conn, server.c_str(), user.c_str(),
             password.c_str(), dbname.c_str(), 3306, nullptr, 0);
          if (p != nullptr)
          {
-             mysql_query(_conn, "set names gbk");
+             mysql_query(m_conn, "set names gbk");
          }
          return p;
     }
     // 更新操作
     bool update(string sql)
     {
-        if (mysql_query(_conn, sql.c_str()))
+        if (mysql_query(m_conn, sql.c_str()))
         {
             LOG_INFO << __FILE__ << ":" << __LINE__ << ":"
                 << sql << "更新失败!";
@@ -54,16 +54,16 @@ class MySQL
     // 查询操作
     MYSQL_RES* query(string sql)
     {
-        if (mysql_query(_conn, sql.c_str()))
+        if (mysql_query(m_conn, sql.c_str()))
         {
             LOG_INFO << __FILE__ << ":" << __LINE__ << ":"
                 << sql << "查询失败!";
             return nullptr;
         }
-        return mysql_use_result(_conn);
+        return mysql_use_result(m_conn);
     }
  private:
-    MYSQL *_conn;
+    MYSQL *m_conn;
  };
 
 
